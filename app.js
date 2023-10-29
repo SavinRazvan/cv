da84a333482b9add80af04aab5f2c5c80e053ac0
SavinRazvan <razvan.savin.work@gmail.com>
2023-10-29 20:50:50 +0200

    -

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const btnEn = document.getElementById("btnEn");
 const btnRo = document.getElementById("btnRo");
-const pdfEmbed = document.getElementById("pdfEmbed");
+const pdfEmbed = document.querySelector("iframe"); // Changed to select the iframe
 const pdfLink = document.getElementById("pdfLink");
 
-btnEn.addEventListener("click", function() {
-    pdfEmbed.data = "Savin_Razvan_EN.pdf";
-    pdfLink.href = "Savin_Razvan_EN.pdf";
-});
+if (btnEn && btnRo && pdfEmbed && pdfLink) {
+    btnEn.addEventListener("click", function() {
+        pdfEmbed.src = "Savin_Razvan_EN.pdf"; // Changed to update the src of the iframe
+        pdfLink.href = "Savin_Razvan_EN.pdf";
+    });
 
-btnRo.addEventListener("click", function() {
-    pdfEmbed.data = "Savin_Razvan_RO.pdf";
-    pdfLink.href = "Savin_Razvan_RO.pdf";
-});
+    btnRo.addEventListener("click", function() {
+        pdfEmbed.src = "Savin_Razvan_RO.pdf"; // Changed to update the src of the iframe
+        pdfLink.href = "Savin_Razvan_RO.pdf";
+    });
 
-// Trigger the "Savin Razvan [RO].pdf" button click on page load
-window.onload = function() {
-    btnRo.click();
-};
+    window.onload = function() {
+        btnRo.click();
+    };
+}
